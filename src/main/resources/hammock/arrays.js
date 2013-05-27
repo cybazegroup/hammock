9fae4a76ea6f84c3938e686f0d805d4ddf2525ae
Roberto Ferranti <rf@emaze.net>
2013-05-27 15:23:56 +0200

    ref: moved String methods to Strings.js

--- src/main/resources/hammock/arrays.js
+++ src/main/resources/hammock/arrays.js
@@ -270,29 +270,3 @@ Array.prototype.flatten = function() {
     }
     return result;
 }
-
-String.prototype.template = function(hash){
-    return this.replace(/{(\w+(?:\.\w+)*)}/g, function(_, query){
-        var pieces = query.split(".");
-        var selected = hash;
-        for(var c=0;c!==pieces.length;++c){
-            selected = selected[pieces[c]];
-        }
-        return selected;
-    })    
-}
-
-String.template = function(str, hash){
-    return str.template(hash);
-}
-
-String.prototype.format = function(){
-    var values = arguments
-    return this.replace(/{(\d+)}/g, function(_, id){
-        return values[+id];
-    })    
-}
-
-String.format = function(str/*, values...*/){
-    return str.format.apply(str, Array.prototype.slice.call(arguments, 1));
-}
