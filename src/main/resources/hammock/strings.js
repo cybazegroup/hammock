b7103f7c1a13887923213efff1c4f67ffe4b7de9
Roberto Ferranti <rf@emaze.net>
2013-08-22 17:26:23 +0200

    enh: [String.template] formatters

--- src/main/resources/hammock/strings.js
+++ src/main/resources/hammock/strings.js
@@ -1,23 +1,27 @@
-String.prototype.template = function(hash){
-    return this.replace(/{(\w+(?:\.\w+)*)}/g, function(_, query){
+String.prototype.contains = function(needle){
+    return this.indexOf(needle) != -1;
+}
+
+String.prototype.template = function(hash, formatters){
+    return this.replace(/{(\w+(?:\.\w+)*)(?::(\w+))?}/g, function(_, query, formatter){
         var pieces = query.split(".");
         var selected = hash;
         for(var c=0;c!==pieces.length;++c){
             selected = selected[pieces[c]];
         }
-        return selected;
-    })    
+        return formatters && formatters[formatter] ? formatters[formatter](selected) : selected;
+    })
 }
 
-String.template = function(str, hash){
-    return str.template(hash);
+String.template = function(str, hash, formatters){
+    return str.template(hash, formatters);
 }
 
 String.prototype.format = function(){
     var values = arguments
     return this.replace(/{(\d+)}/g, function(_, id){
         return values[+id];
-    })    
+    })
 }
 
 String.format = function(str/*, values...*/){
