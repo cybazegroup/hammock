4d72dbe131ab019c923c890ea5dcdfbc1e3b004d
Matteo Asero <matteo.asero@emaze.net>
2014-12-23 10:56:55 +0100

    Enh: added equals method to array prototype.

--- src/main/resources/hammock/arrays.js
+++ src/main/resources/hammock/arrays.js
@@ -335,3 +335,23 @@ Array.prototype.flatMap = function (fn, self) {
         return acc.concat(fn.call(self, v, i));
     }, []);    
 };
+
+//http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
+Array.prototype.equals = function (array) {
+    if (!array)
+        return false;
+    if (this.length !== array.length)
+        return false;
+    for (var i = 0, l=this.length; i < l; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] !== array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+};
